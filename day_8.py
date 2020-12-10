ea93e7ae231513db7353ec3a38f9bc3ee410ae25
JoshGrant5 <joshgg@icloud.com>
2020-12-10 14:14:25 -0800

    Part 2 too low at 839

--- day_8.py
+++ day_8.py
@@ -42,27 +42,93 @@
 
 def main():
 
-  accumulator = 0 # Starting value for the accumulator
-  indexes = [] # keep track of which indexes have been hit. If the index we are looking at is inside this array, we terminate the program
-
   with open('./input/day_8.txt') as f:
     content = f.read().splitlines()
 
-  index = 0
+  def part_one():
+    accumulator = 0 # Starting value for the accumulator
+    indexes = [] # keep track of which indexes have been hit. If the index we are looking at is inside this array, we terminate the program
 
-  while index not in indexes:
-    instruction = content[index][:3]
-    num = int(content[index][3:])
-    indexes.append(index)
-    if instruction == 'acc':
-      accumulator += num
-      index += 1
-    elif instruction == 'jmp':
-      index += num
-    else:
-      index += 1
+    index = 0
 
-  print(accumulator)
+    while index not in indexes:
+      instruction = content[index][:3]
+      num = int(content[index][3:])
+      indexes.append(index)
+      if instruction == 'acc':
+        accumulator += num
+        index += 1
+      elif instruction == 'jmp':
+        index += num
+      else:
+        index += 1
+
+    print(accumulator)
+
+  #   --- Part Two ---
+  # After some careful analysis, you believe that exactly one instruction is corrupted.
+
+  # Somewhere in the program, either a jmp is supposed to be a nop, or a nop is supposed to be a jmp. (No acc instructions were harmed in the corruption of this boot code.)
+
+  # The program is supposed to terminate by attempting to execute an instruction immediately after the last instruction in the file. By changing exactly one jmp or nop, you can repair the boot code and make it terminate correctly.
+
+  # For example, consider the same program from above:
+
+  # nop +0
+  # acc +1
+  # jmp +4
+  # acc +3
+  # jmp -3
+  # acc -99
+  # acc +1
+  # jmp -4
+  # acc +6
+  # If you change the first instruction from nop +0 to jmp +0, it would create a single-instruction infinite loop, never leaving that instruction. If you change almost any of the jmp instructions, the program will still eventually find another jmp instruction and loop forever.
+
+  # However, if you change the second-to-last instruction (from jmp -4 to nop -4), the program terminates! The instructions are visited in this order:
+
+  # nop +0  | 1
+  # acc +1  | 2
+  # jmp +4  | 3
+  # acc +3  |
+  # jmp -3  |
+  # acc -99 |
+  # acc +1  | 4
+  # nop -4  | 5
+  # acc +6  | 6
+  # After the last instruction (acc +6), the program terminates by attempting to run the instruction below the last instruction in the file. With this change, after the program terminates, the accumulator contains the value 8 (acc +1, acc +1, acc +6).
+
+  # Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp). What is the value of the accumulator after the program terminates?
+
+  def part_two():
+    accumulator = 0 # Starting value for the accumulator
+    indexes = [] # keep track of which indexes have been hit. If the index we are looking at is inside this array, we terminate the program
+
+    index = 0
+
+    while index not in indexes:
+      if index < len(content) - 1:
+        instruction = content[index][:3]
+        num = int(content[index][3:])
+        indexes.append(index)
+        if instruction == 'acc':
+          accumulator += num
+          index += 1
+        elif instruction == 'jmp':
+          if content[index + 1][:3] == 'acc' and (int(content[index + 1][3:]) + index) > len(content):
+            index += 1
+          else:
+            index += num
+        else:
+          index += 1
+      else:
+        break
+
+    print(accumulator)
+
+
+  part_one()
+  part_two()
 
 if __name__== "__main__":
   main()
